897b3701aace769630893d085f8b34be5f4f0efe
Óscar Martínez Urango <90833393+OscarMartinezUrango@users.noreply.github.com>
2023-07-05 17:18:03 +0200

    apifront terminado

---
+++ front/src/test/java/es/ufv/dis/final2022/front/APIfront.java
@@ -0,0 +1,16 @@
+package es.ufv.dis.final2022.front;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class APIfront {
+    APIfront serviceAPI = new APIfront();
+
+    @Test
+    void putPersona() {
+//        assertEquals(new ResponseEntity<>(null, HttpStatus.CREATED), serviceAPI.addProducto(new Producto("nombre", "categoria", 3, "0322500532677")));
+    }
+
+
+}
